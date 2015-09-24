a1d63bdc2f7b1e968faf0643f6ed6278c7870cff
Anthony Bui <anthonybui@gmail.com>
2015-09-23 22:06:03 -0400

    Add support for v1.1 media get and update endpoints

--- lib/wordpress/api/media.rb
+++ lib/wordpress/api/media.rb
@@ -3,19 +3,19 @@ require "wordpress/object/media"
 
 module Wordpress::API
   module Media
-    # def get_post(site, post_id, params = {})
-    #   validate_keys! params, [:context] + default_keys
-    #   exec_api(Wordpress::Request.new(:get, "/rest/v1.1/sites/#{site}/posts/#{post_id}", params)) do |json|
-    #     Wordpress::Object::Post.new(self, json)
-    #   end
-    # end
+    def get_media(site, media_id, params = {})
+      # validate_keys! params, [:context] + default_keys
+      exec_api(Wordpress::Request.new(:get, "/rest/v1.1/sites/#{site}/media/#{media_id}", params)) do |json|
+        Wordpress::Object::Media.new(self, json)
+      end
+    end
 
-    # def update_post(site, post_id, data, params = {})
-    #   validate_keys! params, [:context] + default_keys
-    #   exec_api(Wordpress::Request.new(:post, "/rest/v1.1/sites/#{site}/posts/#{post_id}", params, data)) do |json|
-    #     Wordpress::Object::Post.new(self, json)
-    #   end
-    # end
+    def update_media(site, media_id, data, params = {})
+      # validate_keys! params, [:context] + default_keys
+      exec_api(Wordpress::Request.new(:post, "/rest/v1.1/sites/#{site}/media/#{media_id}", params, data)) do |json|
+        Wordpress::Object::Media.new(self, json)
+      end
+    end
 
     def create_media(site, data, params = {})
       # validate_keys! params, [:context] + default_keys
